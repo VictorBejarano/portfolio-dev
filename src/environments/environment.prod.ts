025f3ce2a7f5cbdff5eef9918f0af3246e26bf93
Victor Bejarano <victor.bejarano@sophossolutions.com>
2023-06-03 20:04:52 -0500

    Initial settings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'professional-page',
+    appId: '1:886365543493:web:4a22faa237895e7d37972c',
+    storageBucket: 'professional-page.appspot.com',
+    apiKey: 'AIzaSyBd7u6rdDCn2hTIAWp35SV4SqjBy7oyxko',
+    authDomain: 'professional-page.firebaseapp.com',
+    messagingSenderId: '886365543493',
+    measurementId: 'G-QW3SWV1MFY',
+  },
   production: true
 };
